1f81096e32ff6f90b38aff34041dc32ad1b68c93
Wgmlgz <62064707+Wgmlgz@users.noreply.github.com>
2022-03-26 15:47:45 +0300

    Cmake build system
    I really hate C++ building systems, emcc and cmake

--- zhaba/main.cpp
+++ zhaba/main.cpp
@@ -1,17 +1,19 @@
-#include "em.hpp"
-#include <iostream>
 #include <fstream>
-#include <string>
+#include <iostream>
 #include <sstream>
-/*0*/ #include "../Zhaba-script/src/Parser/Parser.hpp"
-/*1*/ #include "../Zhaba-script/src/Compiler/Compiler.hpp"
-/*2*/ #include "../Zhaba-script/src/TreeLib/TreeLib.hpp"
-/*3*/ #include "../Zhaba-script/src/Interpreter/ToBytecode.hpp"
+#include <string>
+
+#include "../zhaba-script/src/compiler/Compiler.hpp"
+#include "../zhaba-script/src/interpreter/to_bytecode.hpp"
+#include "../zhaba-script/src/parser/parser.hpp"
+#include "../zhaba-script/src/tree_lib/TreeLib.hpp"
+#include "em.hpp"
 
 extern "C" {
 std::string output;
 EMSCRIPTEN_KEEPALIVE void zh_run_main(const char* input) {
-  zhdata.bools["B"] = true;
+  zhdata = ZHDATA();
+  zhdata.flags["B"] = true;
 
   std::stringstream out_stream;
   std::stringstream in_stream;
@@ -21,13 +23,13 @@ EMSCRIPTEN_KEEPALIVE void zh_run_main(const char* input) {
   in_stream << input;
   std::filesystem::path file_path = "main.zh";
   try {
-    zhdata.std_path = "Zhaba-script/std";
+    zhdata.std_path = "zhaba-script/std";
     compileFile(file_path);
     output = out_stream.str();
     std::cout << "output: " << output << std::endl;
   } catch (ParserError err) {
-    std::cout << err.toString();
-    output = err.toString();
+    std::cout << err.what();
+    output = err.what();
   } catch (zhin::RuntimeError err) {
     std::cout << err.what();
     output = err.what();
@@ -44,6 +46,4 @@ EMSCRIPTEN_KEEPALIVE void zh_set_main(const char* str) {
 }
 }
 
-int main() {
-  zh_run_main("");
-}
+int main() { zh_run_main(""); }
